c7d0b0b1f41b5e4d152f38a793c0b668a0759425
NoKiX <112887401+NoKiXD3V@users.noreply.github.com>
2022-09-30 16:28:19 +0200

    Add files via upload

---
+++ nitrogen.py
@@ -0,0 +1,35 @@
+import random, string, os, time
+from colorama import Fore, Style
+from pystyle import Colorate, Colors
+file = open("oxygen_nitros.txt", "w")
+os.system("title Oxygen Nitro Generator | Made by NoKiX")
+def clear():
+    os.system("cls")
+clear()
+
+print(Colorate.Horizontal(Colors.cyan_to_blue, """
+  /$$$$$$                                                   
+ /$$__  $$                                                  
+| $$  \ $$ /$$   /$$ /$$   /$$  /$$$$$$   /$$$$$$  /$$$$$$$ 
+| $$  | $$|  $$ /$$/| $$  | $$ /$$__  $$ /$$__  $$| $$__  $$
+| $$  | $$ \  $$$$/ | $$  | $$| $$  \ $$| $$$$$$$$| $$  \ $$
+| $$  | $$  >$$  $$ | $$  | $$| $$  | $$| $$_____/| $$  | $$
+|  $$$$$$/ /$$/\  $$|  $$$$$$$|  $$$$$$$|  $$$$$$$| $$  | $$
+ \______/ |__/  \__/ \____  $$ \____  $$ \_______/|__/  |__/
+                     /$$  | $$ /$$  \ $$                    
+                    |  $$$$$$/|  $$$$$$/                    
+                     \______/  \______/                     """, 1))
+
+codes = 0
+def nitro(size=16, chars=string.ascii_letters + string.digits):
+    return "".join(random.choice(chars) for _ in range(size))
+print(f"{Fore.CYAN}[Oxygen Generator] {Fore.WHITE}https://discord.gift/{Style.RESET_ALL}"+nitro())
+while(True):
+    if(codes > random.randint(100000000000000000000000000000, 1000000000000000000000000000000000)):
+        print(f"{Fore.CYAN}[Oxygen Generator] {Fore.WHITE}https://discord.gift/{Style.RESET_ALL}"+nitro())
+        file.write("[Oxygen Generator] https://discord.gift/"+nitro()+"\n")
+    else:
+        codes += 1000
+        print(f"{Fore.CYAN}[Oxygen Generator] {Fore.WHITE}https://discord.gift/{Style.RESET_ALL}"+nitro()) 
+        file.write("[Oxygen Generator] https://discord.gift/"+nitro()+"\n")
+time.sleep(1)
